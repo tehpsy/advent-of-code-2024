2b5fdfdfa1b0d60072346e00b017ea637b1641b7
James Baxter <james.baxter@spitfireaudio.com>
2024-12-01 09:25:59 +0000

    Day 1: Solution

--- src/day1.rs
+++ src/day1.rs
@@ -1,13 +1,16 @@
+use std::collections::HashMap;
+
 pub fn run() {
     use std::fs;
 
     let input = fs::read_to_string("inputs/day1.txt").expect("Failed to read input file");
 
-    println!("Distance sum: {:?}", solve(&input));
+    println!("Distance sum: {:?}", solve_sum_distances(&input));
+    println!("Similarity: {:?}", solve_similarity(&input));
 }
 
-pub fn solve(input: &str) -> u32 {
-    let sorted_input: Vec<Vec<u8>> = parse_input(input)
+pub fn solve_sum_distances(input: &str) -> u32 {
+    let sorted_input: Vec<Vec<u32>> = parse_input(input)
         .into_iter()
         .map(|inner| {
             let mut sorted_inner = inner.clone();
@@ -19,7 +22,7 @@ pub fn solve(input: &str) -> u32 {
     sum_distances(sorted_input)
 }
 
-pub fn sum_distances(columns: Vec<Vec<u8>>) -> u32 {
+pub fn sum_distances(columns: Vec<Vec<u32>>) -> u32 {
     let num_rows = columns.first().map_or(0, |row| row.len());
     let mut sum: u32 = 0;
     for x in 0..num_rows {
@@ -29,19 +32,19 @@ pub fn sum_distances(columns: Vec<Vec<u8>>) -> u32 {
     sum
 }
 
-fn parse_input(input: &str) -> Vec<Vec<u8>> {
-    let rows: Vec<Vec<u8>> = input
+fn parse_input(input: &str) -> Vec<Vec<u32>> {
+    let rows: Vec<Vec<u32>> = input
         .lines()
         .map(|line| {
             line.split_whitespace()
-                .map(|x| x.parse::<u8>().expect("Invalid number"))
+                .map(|x| x.parse::<u32>().expect("Invalid number"))
                 .collect()
         })
         .collect();
 
     let num_columns = rows.first().map_or(0, |row| row.len());
 
-    let mut columns: Vec<Vec<u8>> = vec![Vec::new(); num_columns];
+    let mut columns: Vec<Vec<u32>> = vec![Vec::new(); num_columns];
 
     for row in rows {
         for (col_idx, value) in row.into_iter().enumerate() {
@@ -52,12 +55,31 @@ fn parse_input(input: &str) -> Vec<Vec<u8>> {
     columns
 }
 
+pub fn solve_similarity(input: &str) -> u32 {
+    let parsed_input: Vec<Vec<u32>> = parse_input(input);
+    let occurrence_lookup = count_occurrences(parsed_input[1].clone());
+
+    parsed_input[0].iter().fold(0, |acc, &x| {
+        acc + x * occurrence_lookup.get(&x).unwrap_or(&0)
+    })
+}
+
+fn count_occurrences(numbers: Vec<u32>) -> HashMap<u32, u32> {
+    let mut counts = HashMap::new();
+
+    for number in numbers {
+        *counts.entry(number).or_insert(0) += 1;
+    }
+
+    counts
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test_solve() {
+    fn test_part1() {
         let test_input = "3   4
 4   3
 2   5
@@ -65,6 +87,18 @@ mod tests {
 3   9
 3   3";
 
-        assert_eq!(solve(&test_input), 11);
+        assert_eq!(solve_sum_distances(&test_input), 11);
+    }
+
+    #[test]
+    fn test_part2() {
+        let test_input = "3   4
+4   3
+2   5
+1   3
+3   9
+3   3";
+
+        assert_eq!(solve_similarity(&test_input), 31);
     }
 }
